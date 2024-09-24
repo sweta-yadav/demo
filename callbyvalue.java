a25cf647d9490655dad899081870294be33415f1
sweta <swetayadav2060@gmail.com>
2024-09-24 18:48:59 +0545

    Create callbyvalue.java

---
+++ callbyvalue.java
@@ -0,0 +1,13 @@
+public class callbyvalue {
+    public static void main(String[] args) {
+        int a=1000;
+        System.out.println(a);
+        int b;
+        b=a;
+        System.out.println(b);
+        b=2000;
+        System.out.println(b);
+        System.out.println(a);
+
+    } 
+}
